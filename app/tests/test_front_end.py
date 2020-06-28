d8dbd3899414e95a7357ed8d051680873efd6211
greendinosaur <greendinosaur@users.noreply.github.com>
2020-06-27 20:13:42 -0700

    circleci updates

--- app/tests/test_front_end.py
+++ app/tests/test_front_end.py
@@ -22,10 +22,11 @@ class TestBase(LiveServerTestCase):
     def setUp(self):
         """Setup the test driver and create test users"""
         options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
         options.add_argument('--disable-infobars')
         options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--no-sandbox')
         options.add_argument('--disable-setuid-sandbox')
+        options.add_argument("--window-size=1920,1080")
         self.driver = webdriver.Chrome(options=options)
         self.driver.get(self.get_server_url())
 
